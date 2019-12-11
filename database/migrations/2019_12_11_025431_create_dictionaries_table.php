fa4b5c2eb5bc4c9ca2a83ba0ba15a23d2d1f3cbc
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2019-12-11 06:42:17 +0330

    add migrations

---
+++ database/migrations/2019_12_11_025431_create_dictionaries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDictionariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dictionaries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('source_language_id');
+            $table->unsignedBigInteger('target_language_id');
+            $table->string('name');
+            $table->text('details')->nullable();
+            $table->string('author')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dictionaries');
+    }
+}
